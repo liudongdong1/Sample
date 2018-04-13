ace15ab61f443ead84d3c49138a39de1a0f70756
liudongdong1 <3463264078qwer@gmail.com>
2018-04-13 11:30:24 +0800

    采用单例模式封装百度离线语音合成功能

--- app/src/main/java/com/baidu/tts/sample/MainActivity.java
+++ app/src/main/java/com/baidu/tts/sample/MainActivity.java
@@ -12,7 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
 
 
 import java.util.ArrayList;
